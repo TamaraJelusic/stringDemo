ebcebe1434b32f4241c1da60f869fea1c0d87127
Jelušic <tamara.jelusic@accenture.com>
2022-05-17 18:51:08 +0200

    regex examples

--- src/main/java/com/accenture/salsasyntax/Matches.java
+++ src/main/java/com/accenture/salsasyntax/Matches.java
@@ -1,6 +1,24 @@
 package com.accenture.salsasyntax;
 
 public class Matches {
+    //public boolean matches(String regex)
 
+    public static void main(String[] args) {
+        String s = "I ";
+
+        // ^ starting of string
+        // . any character
+        System.out.println(s.matches("^I."));
+
+        String s1 = "I am a string";
+
+        // * zero or more of .
+        System.out.println(s1.matches("^I.*"));
+
+        // $ end of string
+        System.out.println(s1.matches("^I.*f$")); //false
+        System.out.println(s1.matches("^I.*g$")); //true
+
+    }
 
 }
